20d2e3e55f2154d136428a647e62d3eaf97f3a27
John Simmons <jgsimmons42@students.tntech.edu>
2018-09-18 00:26:14 -0500

    9/18/2018 Documents and Article Update

---
+++ public/assets/articles/list/physical_health.js
@@ -0,0 +1,74 @@
+export default {
+  id: '6',
+  topic: 'Life & Wellness',
+  date: '09/17/2018',
+  title: 'Physical Health',
+  image: '../../assets/articles/images/stock_healthy_meal.jpg',
+  content: `
+Physical Health
+===============
+##### 09/17/2018
+
+![Healthy Meal](../../assets/articles/images/stock_healthy_meal.jpg)
+
+Eating a decent diet, achieving an adequate amount of sleep, and committing to a little bit of exercise. These three things go a long way towards maintaining good health. Practicing all three day-after-day is difficult.
+
+Today I will talk about how I handle each topic of physical health: diet, sleep, and exercise. Altogether, they determine the energy with which I go about my day.
+
+### Diet
+
+I eat once a day. Each meal is like a buffet. I eat to my heart's content and then some. That way, I do not go hungry over the next 24 hours or so.
+
+I save money by eating once a day. The school cafeteria charges a flat flee regardless of how much a student eats. Since the flat fee is expensive, I make the most of it by stuffing my face.
+
+Eating one meal a day neither feels unhealthy or healthy. Well, it did feel strange at first. After three weeks of daily meals, my body has grown used to the practice. I no longer hunger between meals. I still have to eat a size-able portion every meal. My stomach does not seem to mind! I may stick with one meal a day even after this semester.
+
+The timing of my meals does not seem to matter as much as the contents of each meal . My University's cafeteria provides a rich variety of foodstuffs—some more healthy than others. The meal I eat at the cafeteria is what I make of it. I can load up on sugar just as much as I can pile on the rice, green beans, and carrots.
+
+I tend to grab pizza and cookies over the healthier alternatives. I love delicious baked goods. I went a couple weeks without eating sugar last semester. The lack of sugar had no noticeable effect on my energy and health, so I went right back to eating sugar. I have been eating cookies ever since.
+
+My energy levels may waiver throughout the day. I attribute this more to a lack of hydration than a unhealthy diet. No doubt about it, drinking water comes first over when and what I eat. Plenty of water with at least one meal a day is all I need.
+
+### Sleep
+
+Food keeps the body warm; sleep keeps it upright and alert. My questionable diet accompanies ~6-7 hours of sleep. While eating cannot fully make-up for a lack of sleep, it does help.
+
+Sleeping happens in two ways: a short nap or an extended period of unconscious shut-eye. My brain can get backlogged with whatever I was doing earlier in the day. Right now, the majority of my day constitutes school and work. By the time I get home, my brain feels jumbled. 15-60 minutes of rest resets my brain for the night.
+
+Each night ends with sleep. My nightlife before then consists of homework and ~3 hours of relaxation. This part of my day radically differs from the morning through early evening. Napping eases the transition into my studious nightlife.
+
+Contrary to best-practice, I leave my bed disheveled. My covers lie in haphazard bundles. As long as my bedding is clean, I disregard the rest. I like having at least two covers because of the increased pressure. I fall asleep much faster with a comfortable amount of pressure over my entire body.
+
+I value the time I sleep more than what I fall asleep on. This explains my disheveled bed. Even so, leaving it so messy is lazy. Why should I start the day off lazy? On second-thought, making my bed makes more sense. Now I got to make it a habit!
+
+I rarely dream in my sleep. My mind must lack those creative bedtime energies during bedtime. I believe dreams can inspire incredible thinking. Unfortunately, I cannot enjoy such thinking with how my body sleeps. Its nothing but black until the alarm clock rings.
+
+I sleep with my back against the bed. I used to sleep on my belly, but I read somewhere that it was unhealthy. Hard to say if I was reading from a reputable source. Sleeping on my belly was an easy way of providing pressure. I use my covers to apply a similar type of pressure so I can still fall asleep.
+
+I spend about a third of my day sleeping. I do my best to do it right  every night for at least 6-7 hours.
+
+### Exercise
+
+Exercise burns food while strengthening mind and body. Physical training has been near-and-dear to my heart ever since high school martial arts. Its benefits greatly outweigh the hardship.
+
+I think of exercise as a means for training self-discipline. I have to set aside the time, struggle through the ordeal, and stretch my body afterwards. Each part of the process goes way against my lazy inclinations.
+
+My laziness gets to me. It is the reason why I fail to exercise most of the time. On the days I do manage some exercise, the work-out only lasts 10-20 minutes.
+
+30 minutes of training daily is my goal. Currently, I am struggling to exercise every other day. As part of writing this article, I intend to exercise daily even if not for the full 30 minutes. The self-discipline I stand to gain will prove invaluable later on.
+
+Of course, exercise requires a fair bit of energy. So long as I sleep and eat, exercise poses no real issue pardoning my laziness.
+
+I mentioned 10-20 minutes of exercise. Those 10-20 minutes consist of circuit training. I maximize work-out intensity by setting 30-second intervals. I perform one exercise for 30 seconds before immediately switching to the next one. After six 30-second intervals, I rest for 30-seconds before repeating the whole process.
+
+This method of training gets my heart pumping. The training puts more strain on my body than running. It also takes less time than running. An average run typically lasts 45-60 minutes. I can circuit train in half the time and achieve similar health results.
+
+That is not to say that circuit training beats running. Each method involves a different set of muscles. In the interest of time and upper-body, I prefer 30-second interval circuit training.
+
+### Conclusion
+
+Diet, sleep, and exercise are the three pillars to my physical health. Today I talked about how I go about each pillar in my life as a University student. My eating habits are weird. My sleep/nap schedule sort of makes sense. I have lots to improve in terms of working out. Moving forward, I intend to curb my laziness and improve my self-discipline. No promises on the cookies I eat.
+
+For next week's article, I plan to create a new category of articles. I want to start writing tutorials on how-to-code. I have not decided on a topic yet. Once I settle on something, expect the next few articles to focus entirely on that subject. Until next Monday!
+`
+};
--- public/main.js
+++ public/main.js
@@ -1551,9 +1551,9 @@ var CacheService = /** @class */ (function () {
             ['BACK_END_CERTIFICATION', { title: 'Free Code Camp Back End Development Program', file: '../../assets/documents/back-end-certification.pdf', format: '../../assets/documents/Logo_PDF.svg' }],
             ['DATA_VISUALIZATION_CERTIFICATION', { title: 'Free Code Camp Data Visualization Program', file: '../../assets/documents/data-visualization-certification.pdf', format: '../../assets/documents/Logo_PDF.svg' }],
             ['FRONT_END_CERTIFICATION', { title: 'Free Code Camp Front End Development Program', file: '../../assets/documents/front-end-certification.pdf', format: '../../assets/documents/Logo_PDF.svg' }],
-            ['COVER_LETTER', { title: 'Cover Letter', file: '../../assets/documents/cover-letter-05202018.pdf', format: '../../assets/documents/Logo_PDF.svg' }],
-            ['RESUME', { title: 'Resume', file: '../../assets/documents/resume-05202018.pdf', format: '../../assets/documents/Logo_PDF.svg' }],
-            ['TRANSCRIPT', { title: 'University Transcript', file: '../../assets/documents/transcript-05202018.pdf', format: '../../assets/documents/Logo_PDF.svg' }],
+            ['COVER_LETTER', { title: 'Cover Letter', file: '../../assets/documents/JOHN_SIMMONS_COVER_09152018.pdf', format: '../../assets/documents/Logo_PDF.svg' }],
+            ['RESUME', { title: 'Resume', file: '../../assets/documents/JOHN_SIMMONS_RESUME_09152018.pdf', format: '../../assets/documents/Logo_PDF.svg' }],
+            ['TRANSCRIPT', { title: 'University Transcript', file: '../../assets/documents/JOHN_SIMMONS_TRANSCRIPT.pdf', format: '../../assets/documents/Logo_PDF.svg' }],
             ['SERVER_SIDE_DEVELOPMENT_CERTIFICATION', { title: 'Server-side Development with NodeJS, Express and MongoDB', file: '../../assets/documents/server-side-certification.pdf', format: '../../assets/documents/Logo_PDF.svg' }]
         ]);
         this.allArticles = Object.values(_assets_articles_index_js__WEBPACK_IMPORTED_MODULE_1__["default"]);
@@ -1804,9 +1804,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _list_example_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./list/example.js */ "./src/assets/articles/list/example.js");
 /* harmony import */ var _list_fall_semester_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./list/fall_semester.js */ "./src/assets/articles/list/fall_semester.js");
 /* harmony import */ var _list_internships_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./list/internships.js */ "./src/assets/articles/list/internships.js");
-/* harmony import */ var _list_scheduling_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./list/scheduling.js */ "./src/assets/articles/list/scheduling.js");
-/* harmony import */ var _list_starting_a_blog_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./list/starting_a_blog.js */ "./src/assets/articles/list/starting_a_blog.js");
-/* harmony import */ var _list_teaching_assistant_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./list/teaching_assistant.js */ "./src/assets/articles/list/teaching_assistant.js");
+/* harmony import */ var _list_physical_health_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./list/physical_health.js */ "./src/assets/articles/list/physical_health.js");
+/* harmony import */ var _list_scheduling_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./list/scheduling.js */ "./src/assets/articles/list/scheduling.js");
+/* harmony import */ var _list_starting_a_blog_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./list/starting_a_blog.js */ "./src/assets/articles/list/starting_a_blog.js");
+/* harmony import */ var _list_teaching_assistant_js__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./list/teaching_assistant.js */ "./src/assets/articles/list/teaching_assistant.js");
+
 
 
 
@@ -1818,9 +1820,10 @@ __webpack_require__.r(__webpack_exports__);
     example: _list_example_js__WEBPACK_IMPORTED_MODULE_0__["default"],
     fall_semester: _list_fall_semester_js__WEBPACK_IMPORTED_MODULE_1__["default"],
     internships: _list_internships_js__WEBPACK_IMPORTED_MODULE_2__["default"],
-    scheduling: _list_scheduling_js__WEBPACK_IMPORTED_MODULE_3__["default"],
-    starting_a_blog: _list_starting_a_blog_js__WEBPACK_IMPORTED_MODULE_4__["default"],
-    teaching_assistant: _list_teaching_assistant_js__WEBPACK_IMPORTED_MODULE_5__["default"]
+    physical_health: _list_physical_health_js__WEBPACK_IMPORTED_MODULE_3__["default"],
+    scheduling: _list_scheduling_js__WEBPACK_IMPORTED_MODULE_4__["default"],
+    starting_a_blog: _list_starting_a_blog_js__WEBPACK_IMPORTED_MODULE_5__["default"],
+    teaching_assistant: _list_teaching_assistant_js__WEBPACK_IMPORTED_MODULE_6__["default"]
 });
 
 
@@ -1998,6 +2001,93 @@ Self-teach yourself as much as possible no matter what you decide. Teaching your
 In College, you should be perfecting your craft so that you can tackle the real world. I understand working retail/fast-food/server positions provide a solid source of income, but what if you could earn that same income while kick-starting your career?
 
 Internships allow for that. I have no regrets about my past two internships. I hope to undertake several more. Nothing is ever guaranteed. Yet I take heart in curving the chances of failure with each passing hour of programming. You can do the same starting today.
+`
+});
+
+
+/***/ }),
+
+/***/ "./src/assets/articles/list/physical_health.js":
+/*!*****************************************************!*\
+  !*** ./src/assets/articles/list/physical_health.js ***!
+  \*****************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ({
+  id: '6',
+  topic: 'Life & Wellness',
+  date: '09/17/2018',
+  title: 'Physical Health',
+  image: '../../assets/articles/images/stock_healthy_meal.jpg',
+  content: `
+Physical Health
+===============
+##### 09/17/2018
+
+![Healthy Meal](../../assets/articles/images/stock_healthy_meal.jpg)
+
+Eating a decent diet, achieving an adequate amount of sleep, and committing to a little bit of exercise. These three things go a long way towards maintaining good health. Practicing all three day-after-day is difficult.
+
+Today I will talk about how I handle each topic of physical health: diet, sleep, and exercise. Altogether, they determine the energy with which I go about my day.
+
+### Diet
+
+I eat once a day. Each meal is like a buffet. I eat to my heart's content and then some. That way, I do not go hungry over the next 24 hours or so.
+
+I save money by eating once a day. The school cafeteria charges a flat flee regardless of how much a student eats. Since the flat fee is expensive, I make the most of it by stuffing my face.
+
+Eating one meal a day neither feels unhealthy or healthy. Well, it did feel strange at first. After three weeks of daily meals, my body has grown used to the practice. I no longer hunger between meals. I still have to eat a size-able portion every meal. My stomach does not seem to mind! I may stick with one meal a day even after this semester.
+
+The timing of my meals does not seem to matter as much as the contents of each meal . My University's cafeteria provides a rich variety of foodstuffs—some more healthy than others. The meal I eat at the cafeteria is what I make of it. I can load up on sugar just as much as I can pile on the rice, green beans, and carrots.
+
+I tend to grab pizza and cookies over the healthier alternatives. I love delicious baked goods. I went a couple weeks without eating sugar last semester. The lack of sugar had no noticeable effect on my energy and health, so I went right back to eating sugar. I have been eating cookies ever since.
+
+My energy levels may waiver throughout the day. I attribute this more to a lack of hydration than a unhealthy diet. No doubt about it, drinking water comes first over when and what I eat. Plenty of water with at least one meal a day is all I need.
+
+### Sleep
+
+Food keeps the body warm; sleep keeps it upright and alert. My questionable diet accompanies ~6-7 hours of sleep. While eating cannot fully make-up for a lack of sleep, it does help.
+
+Sleeping happens in two ways: a short nap or an extended period of unconscious shut-eye. My brain can get backlogged with whatever I was doing earlier in the day. Right now, the majority of my day constitutes school and work. By the time I get home, my brain feels jumbled. 15-60 minutes of rest resets my brain for the night.
+
+Each night ends with sleep. My nightlife before then consists of homework and ~3 hours of relaxation. This part of my day radically differs from the morning through early evening. Napping eases the transition into my studious nightlife.
+
+Contrary to best-practice, I leave my bed disheveled. My covers lie in haphazard bundles. As long as my bedding is clean, I disregard the rest. I like having at least two covers because of the increased pressure. I fall asleep much faster with a comfortable amount of pressure over my entire body.
+
+I value the time I sleep more than what I fall asleep on. This explains my disheveled bed. Even so, leaving it so messy is lazy. Why should I start the day off lazy? On second-thought, making my bed makes more sense. Now I got to make it a habit!
+
+I rarely dream in my sleep. My mind must lack those creative bedtime energies during bedtime. I believe dreams can inspire incredible thinking. Unfortunately, I cannot enjoy such thinking with how my body sleeps. Its nothing but black until the alarm clock rings.
+
+I sleep with my back against the bed. I used to sleep on my belly, but I read somewhere that it was unhealthy. Hard to say if I was reading from a reputable source. Sleeping on my belly was an easy way of providing pressure. I use my covers to apply a similar type of pressure so I can still fall asleep.
+
+I spend about a third of my day sleeping. I do my best to do it right  every night for at least 6-7 hours.
+
+### Exercise
+
+Exercise burns food while strengthening mind and body. Physical training has been near-and-dear to my heart ever since high school martial arts. Its benefits greatly outweigh the hardship.
+
+I think of exercise as a means for training self-discipline. I have to set aside the time, struggle through the ordeal, and stretch my body afterwards. Each part of the process goes way against my lazy inclinations.
+
+My laziness gets to me. It is the reason why I fail to exercise most of the time. On the days I do manage some exercise, the work-out only lasts 10-20 minutes.
+
+30 minutes of training daily is my goal. Currently, I am struggling to exercise every other day. As part of writing this article, I intend to exercise daily even if not for the full 30 minutes. The self-discipline I stand to gain will prove invaluable later on.
+
+Of course, exercise requires a fair bit of energy. So long as I sleep and eat, exercise poses no real issue pardoning my laziness.
+
+I mentioned 10-20 minutes of exercise. Those 10-20 minutes consist of circuit training. I maximize work-out intensity by setting 30-second intervals. I perform one exercise for 30 seconds before immediately switching to the next one. After six 30-second intervals, I rest for 30-seconds before repeating the whole process.
+
+This method of training gets my heart pumping. The training puts more strain on my body than running. It also takes less time than running. An average run typically lasts 45-60 minutes. I can circuit train in half the time and achieve similar health results.
+
+That is not to say that circuit training beats running. Each method involves a different set of muscles. In the interest of time and upper-body, I prefer 30-second interval circuit training.
+
+### Conclusion
+
+Diet, sleep, and exercise are the three pillars to my physical health. Today I talked about how I go about each pillar in my life as a University student. My eating habits are weird. My sleep/nap schedule sort of makes sense. I have lots to improve in terms of working out. Moving forward, I intend to curb my laziness and improve my self-discipline. No promises on the cookies I eat.
+
+For next week's article, I plan to create a new category of articles. I want to start writing tutorials on how-to-code. I have not decided on a topic yet. Once I settle on something, expect the next few articles to focus entirely on that subject. Until next Monday!
 `
 });
 
